cc49443748966cb454c0fbed8c76ba714ff236cf
Anders Geer Jakobsen <geer.anders@gmail.com>
2017-02-08 11:07:55 +0100

    Refactored to triangle class

--- TriangleStuff/Program.cs
+++ TriangleStuff/Program.cs
@@ -6,47 +6,25 @@ namespace TriangleStuff
     internal class Program
     {
 
-        public static string _notTriangle = "This is not a triangle";
-        public static string _equilateralTriangle = "This is an equilateral triangle";
-        public static string _isoscelesTriangle = "This is an isosceles triangle";
-        public static string _scaleneTriangle = "This is a scalene triangle";
+
 
         public static void Main(string[] args)
         {
-
             Console.WriteLine("Enter first number and press enter");
-            int a = int.Parse(Console.ReadLine());
+            var a = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter second number and press enter");
-            int b = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter thrid number and press enter");
-            int c = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(TriangleTest(a,b,c));
+            Triangle t = new Triangle(a,b,c);
+
+            Console.WriteLine(t.TriangleTest());
             Console.ReadKey();
-
-
         }
 
-        public static string TriangleTest(int a, int b, int c)
-        {
-            if (a+b<c || a+c<b || b+c<a)
-            {
-                return _notTriangle;
-            }
-            else if(a == b && a == c)
-            {
-                return _equilateralTriangle;
-            }
-            else if ((a == b && a!= c) || (b == c && b != a))
-            {
-                return _isoscelesTriangle;
-            }
-            else
-            {
-                return _scaleneTriangle;
-            }
-        }
+
     }
 }
---
+++ TriangleStuff/Triangle.cs
@@ -0,0 +1,44 @@
+﻿namespace TriangleStuff
+{
+    public class Triangle
+    {
+        public Triangle(int a, int b, int c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        private readonly int _a;
+        private readonly int _b;
+        private readonly int _c;
+
+        public string NotTriangle { get; } = "This is not a triangle";
+
+        public string EquilateralTriangle { get; } = "This is an equilateral triangle";
+
+        public string IsoscelesTriangle { get; } = "This is an isosceles triangle";
+
+        public string ScaleneTriangle { get; } = "This is a scalene triangle";
+
+        public string TriangleTest()
+        {
+            if (_a+_b<_c || _a+_c<_b || _b+_c<_a)
+            {
+                return NotTriangle;
+            }
+            else if(_a == _b && _b == _c)
+            {
+                return EquilateralTriangle;
+            }
+            else if ((_a == _b && _a!= _c) || (_b == _c && _b != _a))
+            {
+                return IsoscelesTriangle;
+            }
+            else
+            {
+                return ScaleneTriangle;
+            }
+        }
+    }
+}
